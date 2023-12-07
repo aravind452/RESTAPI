26fedfdb96d3326e0f4632ec3ae4ed9e71af856a
aravind452 <aravindaru452@gmail.com>
2023-12-07 21:02:20 +0530

    First commit

---
+++ Rest-API/src/main/java/com/example/RestAPI/controller/ProductController.java
@@ -0,0 +1,51 @@
+package com.example.RestAPI.controller;
+
+
+import com.example.RestAPI.entity.Product;
+import com.example.RestAPI.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+
+    @GetMapping("/addProduct")
+    public Product addProduct(@RequestBody Product product){
+        return service.saveProduct(product);
+    }
+    @GetMapping("/addProducts")
+    public List<Product> addProducts(@RequestBody List<Product> products){
+        return service.saveProducts(products);
+    }
+
+    @GetMapping("/products")
+    public List<Product> findAllProducts(){
+        return service.getProducts();
+    }
+
+    @GetMapping("/product/{id}")
+    public Product findProductById(@PathVariable int id){
+        return service.getProductById(id);
+    }
+
+    @GetMapping("/product/{name}")
+    public Product findProductByName(@PathVariable  String name){
+        return service.getProductByName(name);
+    }
+
+    @PutMapping("/update")
+    public Product updateProduct(@RequestBody Product product){
+        return service.updateProduct(product);
+    }
+    @DeleteMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable  int id){
+        return service.deleteProduct(id);
+    }
+
+}
--- Rest-API/src/main/java/com/example/RestAPI/entity/Product.java
+++ Rest-API/src/main/java/com/example/RestAPI/entity/Product.java
@@ -19,6 +19,41 @@ public class Product {
     @GeneratedValue
     private int id;
     private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
     private int quantity;
     private double price;
+
+
 }
